4d798fabf48e1db8d4bc5b9d3063e77ee531b2a5
chaohang <chaohangz@gmail.com>
2017-01-16 16:09:33 +0800

    添加砸蛋

---
+++ 砸蛋/js/index.js
@@ -0,0 +1,38 @@
+$(function() {
+
+	var dansui = 1;
+	var hammer = $('.hammer')
+	var prize = $('.prize')
+	var egg = $('.egg')
+	var wd = $('.window')
+	egg.click(function() {
+		if (dansui === 0) {
+			alert('蛋都碎了')
+			return
+		}
+		hammer.removeClass('animate')
+		setTimeout(function () {
+			egg.addClass('egg_bg')
+			hammer.hide()
+		}, 500)
+		dansui = 0
+		setTimeout(function () {
+			prize.show()
+					.animate({top: '50px'}, 1200)
+		}, 600)
+		setTimeout(function () {
+			wd.show(300)
+		}, 2000)
+	})
+
+	$('.again').click(function() {
+		$('.window').hide(300)
+		$('.prize').hide()
+							.css('top', '100px')
+		egg.removeClass('egg_bg')
+		$(hammer).css('display', 'block')
+						.addClass('animate')
+		dansui = 1
+	});
+
+})
