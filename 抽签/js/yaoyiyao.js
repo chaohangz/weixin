b1ab97d913bf4faa6d3d2a638234e63c56535a34
chaohangz <chaohangz@gmail.com>
2017-01-24 18:04:43 +0800

    修复摇一摇

--- 抽签/js/yaoyiyao.js
+++ 抽签/js/yaoyiyao.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     if (window.DeviceMotionEvent) {
         var speed = 10;
         var x = y = z = lastX = lastY = lastZ = 0;
+        var flag = 0;
         window.addEventListener('devicemotion', function() {
             var acceleration = event.accelerationIncludingGravity;
             x = acceleration.x;
@@ -9,25 +10,29 @@ $(document).ready(function() {
             z = acceleration.z;
 
             if (Math.abs(x - lastX) > speed || Math.abs(y - lastY) > speed || Math.abs(z - lastZ) > speed) {
-                var $yaoyiyao = $('.yaoyiyao')
-                var $chouqian = $('.chouqian')
-                var $chouqian_img = $('.chouqian_img')
-                var $load = $('.load')
-                var $result = $('.result')
-                var $bg = $('.bg')
-                $yaoyiyao.hide()
-                $chouqian.show()
-                $chouqian_img.show()
-                setTimeout(function() {
-                    $chouqian_img.hide()
-                    $load.show()
-                } , 1000)
-                setTimeout(function() {
-                    $chouqian.hide()
-                    $load.hide()
-                    $bg.addClass('bg_result')
-                    $result.show()
-                }, 2500)
+                if (!flag) {
+                    flag = 1;
+                    var $yaoyiyao = $('.yaoyiyao')
+                    var $chouqian = $('.chouqian')
+                    var $chouqian_img = $('.chouqian_img')
+                    var $load = $('.load')
+                    var $result = $('.result')
+                    var $bg = $('.bg')
+                    $yaoyiyao.hide()
+                    $chouqian.show()
+                    $chouqian_img.show()
+                    setTimeout(function() {
+                        $chouqian_img.hide()
+                        $load.show()
+                    } , 1000)
+                    setTimeout(function() {
+                        $chouqian.hide()
+                        $load.hide()
+                        $bg.addClass('bg_result')
+                        $result.show()
+                        alert('one')
+                    }, 2500)
+                }
             }
             lastX = x;
             lastY = y;
